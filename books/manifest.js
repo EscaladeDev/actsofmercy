88c030809d6838975a3b17fffeb5030fc5c5fd9c
Escalade <78426164+EscaladeDev@users.noreply.github.com>
2025-12-30 13:01:02 -0500

    Update authors and book details in manifest.js

--- books/manifest.js
+++ books/manifest.js
@@ -3,14 +3,14 @@ const libraryData = {
     {
       "file": "Test.epub",
       "name": "Test Book",
-      "author": "Herman Melville",
+      "author": "placeholder",
       "lat": -37.8136, 
       "lng": 144.9631,
       "locationName": "Pacific Ocean"
     },
     {
-      "file": "sherlock.pdf",
-      "name": "Sherlock Holmes",
+      "file": "file.pdf",
+      "name": "Test2",
       "lat": 51.5237, 
       "lng": -0.1585,
       "locationName": "London, UK"
